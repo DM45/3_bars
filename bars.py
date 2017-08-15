94d5f066f47b6077b6ef3a2d2d64627e0dd7e450
DM45 <mda.tomsk@gmail.com>
2017-08-15 23:35:56 +0700

    Исправление ошибок
    Упрощена функция validate_data, изменение названия переменной

--- bars.py
+++ bars.py
@@ -10,28 +10,24 @@ def load_data(filepath):
         return json.load(file_handler)
 
 
-def validate_data(data, longitude, latitude):
+def validate_data(loaded_data, longitude, latitude):
     validate_error = 0
-    if data is None:
+    if loaded_data is None:
         validate_error = 1
         print('Wrong filepath or wrong filename')
     try:
         latitude = float(latitude)
-    except ValueError:
-        validate_error = 1
-        print('Wrong latitude')
-    try:
         longitude = float(longitude)
     except ValueError:
         validate_error = 1
-        print('Wrong longitude')
+        print('Wrong latitude or longitude')
     if not validate_error:
         return 1
 
 
-def get_seats_count_and_name(data):
+def get_seats_count_and_name(loaded_data):
     seats_and_name = [
-        (datas.get("SeatsCount"), datas.get("Name"))for datas in data]
+        (datas.get("SeatsCount"), datas.get("Name"))for datas in loaded_data]
     return seats_and_name
 
 
@@ -43,11 +39,11 @@ def get_smallest_bar(seats_and_name):
     return min(seats_and_name)[1]
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(loaded_data, longitude, latitude):
     coord_diff_and_name = [
         ((math.fabs(float(datas.get("Longitude_WGS84")))- float(longitude))
         + (math.fabs(float(datas.get("Latitude_WGS84")))- float(latitude)), 
-        datas.get("Name")) for datas in data
+        datas.get("Name")) for datas in loaded_data
     ]
     return min(coord_diff_and_name)[1]
 
